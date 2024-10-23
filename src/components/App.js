0d3787bbd24a6a23d9d708201e9005d2f1674b73
Ashishkalbande11 <ashishkalbande6@gmail.com>
2024-10-23 08:37:24 +0530

    todo

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,63 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import { v4 as uuidv4 } from 'uuid';
 
 const App = () => {
+  const [todo, setTodo] = useState([]);
+  const [text, setText] = useState("");
+
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      {/* Do not remove the main div */}
+      <div>
+        <h2>To-DO List</h2>
+        <div>
+          <input
+            value={text}  // Set input value to the text state
+            onChange={(e) => {
+              setText(e.target.value);
+            }}
+            type="text"
+            placeholder="Add task"
+          />
+          <button
+            onClick={() => {
+              if (!text) return;
 
-export default App
+              const newTodo = {
+                task: text,
+                id: uuidv4(),
+              };
+
+              setTodo([...todo, newTodo]);
+              setText("");  // Clear the input field by setting text state to empty
+            }}
+          >
+            Add Todo
+          </button>
+        </div>
+      </div>
+      <div>
+        <ul>
+          {todo.map((item) => {
+            return (
+              <li style={{ listStyle: "none" }} key={item.id}>
+                {item.task}{" "}
+                <button
+                  onClick={() => {
+                    const newTodoList = todo.filter((todoItem) => todoItem.id !== item.id);
+                    setTodo(newTodoList);
+                  }}
+                >
+                  Delete
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default App;
